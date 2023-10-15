ae9d6667dc5bd0ca75565d52e1a7fc46ff5beec3
maste <maste@ERYK-DELL>
2023-10-15 14:11:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please enter a name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello " + name + ". I fucking hate you and I don't give a shit on your life.");
+        Console.WriteLine("I think that your face Is ugly and I think that you should go kill yourself now!");
+        Console.WriteLine("Fuck You!");
+
+        Console.ReadLine();
+    }
+}
